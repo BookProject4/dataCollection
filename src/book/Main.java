1979e7787aeee4d82ef1e62050f38f468451024a
joo <joo@192.168.0.16>
2021-09-06 15:52:40 +0900

    update:joo

--- src/book/Main.java
+++ src/book/Main.java
@@ -1,7 +1,10 @@
 package book;
 
+import java.util.Date;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
@@ -9,43 +12,52 @@ public class Main {
 		Main C=new Main();
 		C.getData();
 	}
-
+	
 	public void getData() {
 		BookDAO dao = new BookDAO();
 		try {
-			// 국내
-			// 1 유아 : https://www.aladin.co.kr/shop/wbrowse.aspx?CID=13789
-			// 2 >> 주제별 책읽기 : https://www.aladin.co.kr/shop/wbrowse.aspx?BrowseTarget=List&SortOrder=2&ViewType=Detail&ViewRowsCount=25&PublishMonth=0&PublishDay=84&UsedShop=0&DiscountPercent=20&BranchType=1&Stockstatus=1&CID=70125
+			//isbn,category,subCategory,poster,title,subtitle,writer,publisher,etcInfo,price,discount,text,imgs,contentsTable,tags,publicationDay
 			
-			// 외국
-			// 1 소설/시/희곡 : https://www.aladin.co.kr/shop/wbrowse.aspx?CID=90842
-			// 2 >> 문학 : https://www.aladin.co.kr/shop/wbrowse.aspx?ViewType=Simple&ViewRowsCount=24&PublishMonth=0&PublishDay=84&UsedShop=&DiscountPercent=20&BranchType=1&Stockstatus=1&CID=97454
+			Document doc = Jsoup.connect("https://www.aladin.co.kr/home/welcome.aspx").get();
+			Elements link = doc.select(".browse_sub a");
+			System.out.println("카테고리 개수 : "+link.size());
 			
-			// 이북
-			// 1 유아 : https://www.aladin.co.kr/shop/wbrowse.aspx?CID=38424
-			// 2 >> 0~3세 https://www.aladin.co.kr/shop/wbrowse.aspx?BrowseTarget=List&ViewRowsCount=25&ViewType=Detail&PublishMonth=0&SortOrder=2&Stockstatus=1&PublishDay=84&CustReviewRankStart=&CustReviewRankEnd=&CustReviewCountStart=&CustReviewCountEnd=&PriceFilterMin=&PriceFilterMax=&SearchOption=&NotFreeSampleEbook=&IsRentable=&EbookSerial=&UsedShop=0&DiscountPercent=20&BranchType=1&CID=39420
-			
-			String[] cid= {"13789","90842","38424"};//158585 -> 이북 끝
-			String[] cate= {"국내도서","해외도서","eBook"};
-			Document doc = Jsoup.connect("https://www.aladin.co.kr/shop/wbrowse.aspx?+").get();
-			Elements link = doc.select("a[href*=www.aladin.co.kr/shop/wbrowse.aspx?][class=gr03]");
-			System.out.println("카테고리 내 상세페이지 개수 : "+link.size());
-			
-			int j=0;
 			for(int i=0;i<link.size();i++) {
-				System.out.println(link.get(i));
-				if(j==3)	break;
-				String CID="";
-				
-				
-				BookVO vo=new BookVO();
-				//isbn,category,subCategory,poster,title,subtitle,writer,publisher,etcInfo,price,discount,text,imgs,contentsTable,tags,publicationDay
-				vo.setCategory(cate[j]);
-				
-				
-				if(CID.equals("90842"))	j++;
-				if(CID.equals("38424"))	j++;
-				if(CID.equals("158585"))  j++;
+				//System.out.println("1 : "+link.get(i));
+				//System.out.println("2 : "+link.get(i).attr("href"));
+				for(int p=1;;p++) {
+					Document doc1= Jsoup.connect("https://www.aladin.co.kr"+link.attr("href")+"&BrowseTarget=List&page="+p).get();
+					Elements detailLinks = doc1.select(".ss_book_list a[class=bo3]");
+					if(detailLinks.size()==0)	break;
+					for(int j=0;j<detailLinks.size();j++) {
+						//System.out.println("3 : "+detailLinks.get(j).attr("href"));
+						Document doc2= Jsoup.connect(detailLinks.get(j).attr("href")).get();
+						
+						Element isbn=null;
+						//카테고리 다받아오고 처음꺼의 첫번째는 카테고리에 넣고, 두번째는 서브카테고리에 넣기 (구분자 > )
+						Elements categories=null;
+						String cate1="";
+						String cate2="";
+						Element poster=null;//찾아보기 포스터 어느부분에서 구하는지??
+						Element title=null;
+						Element subTitle=null;
+						//지은이, 출판사, 출판일 묶여있음 ([class=Ere_sub2_title])
+						Elements etc=null;
+						String date="";
+						Date publicationDay=null;//출판일 -> 스트링을 데이트로 바꾸기
+						String writer="";
+						String publisher="";
+						Element etcInfo=null;
+						Element price=null;
+						Element discount=null;
+						//태그 여러개 구분자는 무엇으로 할지 생각하고 저장해야됨
+						Elements tags=null;
+						//이미지파일링크들 어떤 구분자로 나누어서 가져올지 생각 (공백??)
+						Elements imgs=null;
+						//text,contentsTable
+					}
+					
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
