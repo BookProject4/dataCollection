91c8559335ba46a4111e7fdaa60c26ce21612f70
joo <joo@192.168.0.16>
2021-09-06 17:29:18 +0900

    update:joo

--- src/book/Main.java
+++ src/book/Main.java
@@ -16,8 +16,6 @@ public class Main {
 	public void getData() {
 		BookDAO dao = new BookDAO();
 		try {
-			//isbn,category,subCategory,poster,title,subtitle,writer,publisher,etcInfo,price,discount,text,imgs,contentsTable,tags,publicationDay
-			
 			Document doc = Jsoup.connect("https://www.aladin.co.kr/home/welcome.aspx").get();
 			Elements link = doc.select(".browse_sub a");
 			System.out.println("카테고리 개수 : "+link.size());
@@ -32,29 +30,52 @@ public class Main {
 					for(int j=0;j<detailLinks.size();j++) {
 						//System.out.println("3 : "+detailLinks.get(j).attr("href"));
 						Document doc2= Jsoup.connect(detailLinks.get(j).attr("href")).get();
+						/*
+						 * isbn >  .conts_info_list1 li:last-child
+category > #ulCategory a:nth-child(1)
+subCategory > #ulCategory a:nth-child(2)
+						 */
 						
-						Element isbn=null;
-						//카테고리 다받아오고 처음꺼의 첫번째는 카테고리에 넣고, 두번째는 서브카테고리에 넣기 (구분자 > )
-						Elements categories=null;
-						String cate1="";
-						String cate2="";
-						Element poster=null;//찾아보기 포스터 어느부분에서 구하는지??
-						Element title=null;
-						Element subTitle=null;
-						//지은이, 출판사, 출판일 묶여있음 ([class=Ere_sub2_title])
-						Elements etc=null;
-						String date="";
-						Date publicationDay=null;//출판일 -> 스트링을 데이트로 바꾸기
-						String writer="";
-						String publisher="";
-						Element etcInfo=null;
-						Element price=null;
-						Element discount=null;
-						//태그 여러개 구분자는 무엇으로 할지 생각하고 저장해야됨
-						Elements tags=null;
-						//이미지파일링크들 어떤 구분자로 나누어서 가져올지 생각 (공백??)
-						Elements imgs=null;
-						//text,contentsTable
+						BookVO vo=new BookVO();
+						
+						String isbn=doc2.select(".conts_info_list1 li:last-child").text();
+						//System.out.println(isbn);
+						isbn=isbn.substring(isbn.lastIndexOf(":")+2);
+						//System.out.println(isbn);
+						vo.setIsbn(Long.parseLong(isbn));
+						
+						String cate1=doc2.select("#ulCategory a:nth-child(1)").text();
+						cate1=cate1.substring(0,cate1.indexOf(" "));
+						//System.out.println(cate1);
+						vo.setCategory(cate1);
+						
+						String cate2=doc2.select("#ulCategory a:nth-child(2)").text();
+						cate2=cate2.substring(0,cate2.indexOf(" "));
+						//System.out.println(cate2);
+						vo.setSubCategory(cate2);
+						
+						String etcInfo=doc2.select(".conts_info_list1").text();
+						etcInfo=etcInfo.substring(0,etcInfo.lastIndexOf(" ISBN"));
+						//System.out.println(etcInfo);
+						vo.setEtcInfo(etcInfo);
+						
+//						String poster=null;//찾아보기 포스터 어느부분에서 구하는지??
+//						String title=null;
+//						String subTitle=null;
+//						//지은이, 출판사, 출판일 묶여있음 ([class=Ere_sub2_title])
+//						Elements etc=null;
+//						String date="";
+//						Date publicationDay=null;//출판일 -> 스트링을 데이트로 바꾸기
+//						String writer="";
+//						String publisher="";
+//						Element price=null;//가격!!
+//						Element discount=null;//할인가!!
+//						//태그 여러개 구분자는 무엇으로 할지 생각하고 저장해야됨
+//						Elements tags=null; //태그들!!
+//						//이미지파일링크들 어떤 구분자로 나누어서 가져올지 생각 (공백??)
+//						Elements imgs=null; //책 이미지들!!
+//						Elements text=doc2.select(""); // 책소개!!
+//						Elements contentsTable // 목차!
 					}
 					
 				}
